b035b8e077ed2ddc24c2dc53411c9dc2e33b08b9
Brian Mason <brian.kw.mason@gmail.com>
2022-06-19 16:47:54 -0700

    navbar created

---
+++ src/components/navbar/links.data.js
@@ -0,0 +1,17 @@
+export const linkData = [
+  {
+    id: 1,
+    name: "about",
+    link: ".aboutContainer",
+  },
+  {
+    id: 2,
+    name: "projects",
+    link: ".projectsContainer",
+  },
+  {
+    id: 3,
+    name: "contact",
+    link: ".contactContainer",
+  },
+];
