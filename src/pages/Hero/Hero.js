ff1f33fded2790215f2c826f5497407537590cdf
Brian Mason <brian.kw.mason@gmail.com>
2024-01-08 20:48:54 -0500

    revert

--- src/pages/Hero/Hero.js
+++ src/pages/Hero/Hero.js
@@ -13,7 +13,7 @@ export const Hero = () => {
           ease: "easeOut",
         }}
       >
-        Programmer<Highlighter>, Founder, 18 yo</Highlighter>.
+        Programmer<Highlighter>, Founder.</Highlighter>.
       </Tagline>
     </HeroContainer>
   );
