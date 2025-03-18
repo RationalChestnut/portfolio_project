b875e3e19d26b29d7b690e0993a90737468759bb
Rational Chestnut <brian.kw.mason@gmail.com>
2025-03-18 13:30:09 -0400

    Fix

--- src/pages/Content.js
+++ src/pages/Content.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Contact } from "./Contact/Contact";
 import { ContentContainer } from "./Content.styles";
-import { Hero } from "./Hero/Hero";
 import { Projects } from "./Projects/Projects";
 
 export const Content = () => {
